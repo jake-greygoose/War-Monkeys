74578951d6ae01247b3f727b8fb985bf0ed4e726
jake-greygoose <74213373+jake-greygoose@users.noreply.github.com>
2020-11-25 15:33:11 +0000

    add prettifier

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const dotenv = require('dotenv').config()
-const Discord = require('discord.js');
+const dotenv = require("dotenv").config();
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const admin = require("firebase-admin");
-var moment = require('moment-timezone');
-const _ = require('underscore');
-const loggly = require('loggly');
+var moment = require("moment-timezone");
+const _ = require("underscore");
+const loggly = require("loggly");
 
 var Logg = loggly.createClient({
-    token: process.env.LOGGLY_TOKEN,
-    subdomain: "warmonkeys",
-    auth: {
-        username: "warmonkeys",
-        password: process.env.LPASS
-    },
-    //
-    // Optional: Tag to send with EVERY log message
-    //
-    tags: ['global-tag']
+  token: process.env.LOGGLY_TOKEN,
+  subdomain: "warmonkeys",
+  auth: {
+    username: "warmonkeys",
+    password: process.env.LPASS,
+  },
+  //
+  // Optional: Tag to send with EVERY log message
+  //
+  tags: ["global-tag"],
 });
 
-
-client.once('ready', () => {
-
-    console.log('Client Started.')
-    //client.user.setActivity(`Use ${prefix}help.`)
-    //Logg.log('Client Started.');
-
-
+client.once("ready", () => {
+  console.log("Client Started.");
+  // client.user.setActivity(`Use ${prefix}help.`)
+  // Logg.log('Client Started.');
 });
 
 client.login(process.env.TOKEN);
