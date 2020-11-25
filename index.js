80593c3a1f3cd24b54c296fc387f2dfaefca0844
jake-greygoose <74213373+jake-greygoose@users.noreply.github.com>
2020-11-25 14:46:50 +0000

    add eslint

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ var moment = require('moment-timezone');
 const _ = require('underscore');
 const loggly = require('loggly');
 
- client.once('ready', () => {
-   
-   console.log('Client Started.')
-    //client.user.setActivity(`Use ${prefix}help.`)
-   //Logg.log('Client Started.');
- 
- 
- });
+var Logg = loggly.createClient({
+    token: process.env.LOGGLY_TOKEN,
+    subdomain: "warmonkeys",
+    auth: {
+        username: "warmonkeys",
+        password: process.env.LPASS
+    },
+    //
+    // Optional: Tag to send with EVERY log message
+    //
+    tags: ['global-tag']
+});
 
- client.login(process.env.TOKEN);
+
+client.once('ready', () => {
+
+    console.log('Client Started.')
+    //client.user.setActivity(`Use ${prefix}help.`)
+    //Logg.log('Client Started.');
+
+
+});
+
+client.login(process.env.TOKEN);
